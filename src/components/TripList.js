234de96c1692549ebc9fd78c7b719d86b59f700f
Mahmoud <mahmoudabdulmuty@gmail.com>
2022-01-11 04:08:27 +0200

    Creating a Custom Fetch Hook

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -1,30 +1,21 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useState } from 'react';
+import { useFetch } from '../hooks/useFetch';
 import './TripList.css';
 
 export default function TripList() {
-	const [trips, setTrips] = useState([]);
 	const [url, setUrl] = useState('http://localhost:3000/trips');
-
-	const fetchedFunc = useCallback(async () => {
-		const response = await fetch(url);
-		const data = await response.json();
-		setTrips(data);
-	}, [url]);
-
-	useEffect(() => {
-		fetchedFunc();
-	}, [fetchedFunc]);
-
+	const { data: trips } = useFetch(url);
 	return (
 		<div className="trip-list">
 			<h2>Trip List</h2>
 			<ul>
-				{trips.map((trip) => (
-					<li key={trip.id}>
-						<h3>{trip.title}</h3>
-						<p>{trip.price}</p>
-					</li>
-				))}
+				{trips &&
+					trips.map((trip) => (
+						<li key={trip.id}>
+							<h3>{trip.title}</h3>
+							<p>{trip.price}</p>
+						</li>
+					))}
 			</ul>
 			<div className="filters">
 				<button
